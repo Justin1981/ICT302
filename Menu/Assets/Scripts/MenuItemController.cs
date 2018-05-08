b40d49ec669d4850e4359e80471cf0b64aa05b18
Ilja Shevelyov <iljashevelyov@yahoo.co.nz>
2018-05-08 16:09:43 +0800

    -Menu component of game COMPLETE -Menu Builder and menu manager     -Allows for creation of custom menu buttoms -Menu interface
    TODO:
    -Add interactivity for buttons
        -i.e. what button should do

---
+++ Menu/Assets/Scripts/MenuItemController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class MenuItemController : MonoBehaviour, IInputClickHandler {
+
+    private TextMesh _textMesh;
+    private IMenuItemData _menuItemData;
+
+    public IMenuItemData MenuItemData
+    {
+        get { return _menuItemData; }
+        set
+        {
+            if(_menuItemData == value)
+            {
+                return;
+            }
+            _menuItemData = value;
+            _textMesh = GetComponentInChildren<TextMesh>();
+            if(_menuItemData != null && _textMesh != null)
+            {
+                _textMesh.text = _menuItemData.Title;
+            }
+        }
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        if(MenuItemData != null)
+        {
+            PlayConfirmationSound();
+        }
+    }
+
+    private AudioSource _audioSource;
+    private void PlayConfirmationSound()
+    {
+        if(_audioSource == null)
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+        if(_audioSource != null)
+        {
+            _audioSource.Play();
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Menu/Assets/Scripts/Selector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnSelect()
+    {
+        //var hit = HoloToolkit.Unity.GazeManager.Instance.HitInfo;
+        //var hit = HoloToolkit.Unity;
+    }
+}
