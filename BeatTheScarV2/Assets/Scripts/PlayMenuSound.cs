cca9ea6da861a21128837315c8e9affc797dc365
Ilja Shevelyov <shevelyovilja@gmail.com>
2018-05-21 11:24:04 +0800

    CHANGELOG: -Added sound effects for menu TODO: -Test on hololens

---
+++ BeatTheScarV2/Assets/Scripts/PlayMenuSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMenuSound : MonoBehaviour {
+
+    public AudioSource source;
+
+	void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+	
+	public void OnHover()
+    {
+        source.Play();
+    }
+}
